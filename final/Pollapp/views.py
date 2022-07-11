fb89a98f421742403f9d343b64acf3ecd8b931ab
Antoni <antonstrasz@gmail.com>
2022-07-11 15:29:36 +0200

    Created index view. Tried many diffrend methods but this one works the best

--- final/Pollapp/views.py
+++ final/Pollapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    return render(request, "Pollapp/layout.html")
+    return render(request, "Pollapp/index.html")
