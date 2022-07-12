50bb2da22409e4c4a18dcb68b6ff1598063d3eed
Antoni <antonstrasz@gmail.com>
2022-07-12 12:22:38 +0200

    User can now register

--- final/Pollapp/views.py
+++ final/Pollapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import User
 
 # Create your views here.
 def index (request):
@@ -6,11 +10,27 @@ def index (request):
 
 def register (request):
     if request.method == "POST":
-        pass
+        # Gets both passwords value and checks if they match
+        password = request.POST["password"]
+        password_confirmation = request.POST["password_confirmation"]
+        if password != password_confirmation:
+            return render(request, "Pollapp/register.html")
+
+        #Gets both username and email from user input
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except:
+            return render(request, "Pollapp/register.html")
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
     elif request.method == "GET":   
         return render(request, "Pollapp/register.html")
 
-def login (request):
+def login_page (request):
     if request.method == "POST":
         pass
     elif request.method == "GET":   
