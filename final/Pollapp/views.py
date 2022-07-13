312145820e927e1d56ff6cd52f85a74809773782
Antoni <antonstrasz@gmail.com>
2022-07-13 13:13:04 +0200

    login_required() noww works as intended

--- final/Pollapp/views.py
+++ final/Pollapp/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 from .models import User
 
 # Create your views here.
 def index (request):
     return render(request, "Pollapp/index.html")
 
+@login_required(redirect_field_name='login')
 def profile(request):
     return render(request, "Pollapp/profile.html")
 
