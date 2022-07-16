e874a29f9efe06db57f37fea9364eff308bf9139
Antoni <antonstrasz@gmail.com>
2022-07-16 17:43:06 +0200

    Created first version of models, not sure if it will work as intended, so it will be changed later probably

--- final/Pollapp/admin.py
+++ final/Pollapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from  .models import User
+from  .models import User, Poll, Option, Votes
 
 admin.site.register(User)
+admin.site.register(Poll)
+admin.site.register(Option)
+admin.site.register(Votes)
--- final/Pollapp/models.py
+++ final/Pollapp/models.py
@@ -1,8 +1,38 @@
+from re import T
+from tkinter import CASCADE
+from tkinter.messagebox import QUESTION
 from django.db import models
 
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
 
 class User(AbstractUser):
     def __str__(self):
         return f"{self.username}"
+
+class Option(models.Model):
+    question = models.CharField(max_length=64)
+    def __str__(self):
+        return f"{self.question}"
+    
+class Votes(models.Model):
+    voter = models.ForeignKey(User,on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.voter}, voted on {self.option}"
+
+class Poll(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll_question = models.CharField(max_length=64)
+    options = models.ManyToManyField(Option, symmetrical=False, blank=True , related_name="options")
+    votes = models.ManyToManyField(Votes, symmetrical=False, blank=True, related_name="votes")
+    likes = models.ManyToManyField(User, symmetrical=False, blank=True, related_name="likes")
+    private = models.BooleanField()
+    def __str__(self):
+        return f"{self.poll_question}"
+
+
+
