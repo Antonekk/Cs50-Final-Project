d707cd42590c8c585fe5d5e4f26d28d95c510cc2
Antoni <antonstrasz@gmail.com>
2022-07-18 14:05:51 +0200

    Create_poll page frontend form created, React added and tested

---
+++ final/Pollapp/static/Pollapp/react.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function Hello() {
+    return(
+        <div>
+            <h1>Hello</h1> 
+        </div>
+    );
+}
+ReactDOM.render(<Hello />, document.querySelector("#test"));
