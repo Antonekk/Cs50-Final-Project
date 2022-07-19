8e82a9ec4217ed0a6ec503c468b5ca071edac4b6
Antoni <antonstrasz@gmail.com>
2022-07-19 13:59:47 +0200

    create_poll forntend works now

--- final/Pollapp/static/Pollapp/frontend_code.js
+++ final/Pollapp/static/Pollapp/frontend_code.js
@@ -2,12 +2,14 @@ let i = 3;
 
 function addField(){
     if (i <=10){
+        const adddelete = document.getElementById("add_and_delete").style.display = "block"
         const answer = document.createElement('div');
         answer.className = "field"
+        answer.id = `answer${i}`
         answer.innerHTML = `
         <label class="label">Answer #${i}</label>
         <div class="control">
-            <input id="answer${i}"class="input" type="text" placeholder="Enter your answer"></input>
+            <input id="answer_input${i}"class="input" type="text" placeholder="Enter your answer"></input>
         </div>`;
         document.getElementById("answers").append(answer);
         i++;
@@ -15,6 +17,18 @@ function addField(){
     else{
         alert("Can't add more answers")
     }
+    
+}
+
+function removeField(){
+    if (i>3){
+        i--;
+        document.getElementById(`answer${i}`).remove();
+        if (i == 3){
+            document.getElementById("add_and_delete").style.display = "none";
+        }
+    }
+    
     
 }
 
