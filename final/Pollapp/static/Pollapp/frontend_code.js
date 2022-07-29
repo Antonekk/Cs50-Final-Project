7e6a29aace0632c7370f5fd4f452be8d24d72339
Antoni <antonstrasz@gmail.com>
2022-07-29 13:12:49 +0200

    Delete answer animation added

--- final/Pollapp/static/Pollapp/frontend_code.js
+++ final/Pollapp/static/Pollapp/frontend_code.js
@@ -4,7 +4,7 @@ function addField(){
     if (i <=10){
         const adddelete = document.getElementById("add_and_delete").style.display = "block"
         const answer = document.createElement('div');
-        answer.className = "field"
+        answer.className = "field disappear_animation"
         answer.id = `answer${i}`
         answer.innerHTML = `
         <label class="label">Answer #${i}</label>
@@ -23,7 +23,12 @@ function addField(){
 function removeField(){
     if (i>3){
         i--;
-        document.getElementById(`answer${i}`).remove();
+        const option = document.getElementById(`answer${i}`);
+        option.style.animationPlayState = 'running';
+        option.addEventListener('animationend', () => {
+            option.remove();
+        });
+        
         if (i == 3){
             document.getElementById("add_and_delete").style.display = "none";
         }
