393ee92fd65d95bd8731116c7cfd3ef5f1da9a9e
Antoni <antonstrasz@gmail.com>
2022-07-31 17:55:09 +0200

    showing new answer animation added

--- final/Pollapp/static/Pollapp/frontend_code.js
+++ final/Pollapp/static/Pollapp/frontend_code.js
@@ -2,16 +2,21 @@ let i = 3;
 
 function addField(){
     if (i <=10){
-        const adddelete = document.getElementById("add_and_delete").style.display = "block"
+        const adddelete = document.getElementById("add_and_delete").style.display = "block";
         const answer = document.createElement('div');
-        answer.className = "field disappear_animation"
-        answer.id = `answer${i}`
+        answer.className = "field show_animation";
+        answer.id = `answer${i}`;
         answer.innerHTML = `
         <label class="label">Answer #${i}</label>
         <div class="control">
             <input id="answer_input${i}"class="input" type="text" placeholder="Enter your answer"></input>
         </div>`;
         document.getElementById("answers").append(answer);
+        const option = document.getElementById(`answer${i}`);
+        option.style.animationPlayState = 'running';
+        option.addEventListener('animationend', () => {
+            option.style.animationPlayState = 'paused';
+        });
         i++;
     }
     else{
@@ -24,6 +29,7 @@ function removeField(){
     if (i>3){
         i--;
         const option = document.getElementById(`answer${i}`);
+        option.className = 'field disappear_animation';
         option.style.animationPlayState = 'running';
         option.addEventListener('animationend', () => {
             option.remove();
